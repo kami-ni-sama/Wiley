84e4fd0b97f1a1c80dfe6cb672cfe0f62a50a7b5
kami-ni-sama <ahmadjs310599@gmail.com>
2021-07-09 13:12:24 +0530

    Completed second task

--- MiniProject2/Main2.java
+++ MiniProject2/Main2.java
@@ -79,12 +79,13 @@ public class Main2 {
 	            public int compare(Map.Entry<Address,ArrayList<NewUser>> e1, Map.Entry<Address,ArrayList<NewUser>> e2) {
 	                int res = e2.getValue().size() - e1.getValue().size();
 	                if(res == 0) {
-	                	if(e1.getKey().city == e2.getKey().city) 
-	                		return e1.getKey().zipcode.compareTo(e2.getKey().zipcode);
-	                	else
+	                	if(e1.getKey().zipcode.equals(e2.getKey().zipcode)) 
 	                		return e1.getKey().city.compareTo(e2.getKey().city);
+	                	else
+	                		return e1.getKey().zipcode.compareTo(e2.getKey().zipcode);
 	                }
-	                return res != 0 ? res : 1;
+	                return res;
+//	                return res != 0 ? res : 1;
 	            }
 	        }
 	    );
