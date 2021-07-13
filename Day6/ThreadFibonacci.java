32923241b1c27cebf788ce460d452c5f3ee5c785
kami-ni-sama <ahmadjs310599@gmail.com>
2021-07-13 10:58:44 +0530

    Completed fibonacci working properly

--- Day6/ThreadFibonacci.java
+++ Day6/ThreadFibonacci.java
@@ -4,6 +4,10 @@ public class ThreadFibonacci implements Runnable {
 	private int x;
 	public  int answer;
 	public static Map<List<Integer>, Integer> fib = new LinkedHashMap<>();
+	{
+		ThreadFibonacci.fib.put(Arrays.asList(0, 1), 1);
+	}
+	
 	public ThreadFibonacci(int x) {
 		this.x = x;
 	}
@@ -33,7 +37,7 @@ public class ThreadFibonacci implements Runnable {
 	{
 		try {
 
-			for (int i=0;i<5;i++){
+			for (int i=1;i<=5;i++){
 				ThreadFibonacci f= new ThreadFibonacci(i);
 				Thread threadf= new Thread(f);
 				threadf.start();
@@ -42,7 +46,7 @@ public class ThreadFibonacci implements Runnable {
 
 			}
 			for(Map.Entry<List<Integer>, Integer> m:ThreadFibonacci.fib.entrySet()) {
-				System.out.println(m.getKey()+" "+m.getValue());
+				System.out.println(m.getKey().get(0)+" + "+m.getKey().get(1)+" = "+m.getValue());
 			}
 		}
 
