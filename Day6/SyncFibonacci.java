762cca3890689b5ba82715f3bca682c04ca9db69
kami-ni-sama <ahmadjs310599@gmail.com>
2021-07-14 11:27:15 +0530

    Tried using interrupt()

--- Day6/SyncFibonacci.java
+++ Day6/SyncFibonacci.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class SyncFibonacci {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException{
 		List<Integer> fib = new ArrayList<Integer>();
 		int n = 10;
 		SyncFibonacciWriter write = new SyncFibonacciWriter(fib, n);
@@ -13,6 +13,11 @@ public class SyncFibonacci {
 		Thread reader = new Thread(read, "Reader");
 		writer.start();
 		reader.start();
+		writer.join();
+		reader.join();
+//		writer.interrupt();
+//		reader.interrupt();
+		System.out.println("Printed Fibonacci");
 	}
 
 }
@@ -21,14 +26,16 @@ class SyncFibonacciWriter implements Runnable{
 	private List<Integer> arr;
 	private int n;
 	int a = 0, b = 1, c=0;
-	{
-		arr = new ArrayList<Integer>();
-		arr.add(a);
-		arr.add(b);
-	}
+//	{
+//		arr = new ArrayList<Integer>();
+//		arr.add(a);
+//		arr.add(b);
+//	}
 	
 	public SyncFibonacciWriter(List<Integer> arr, int n){
 		this.arr = arr;
+		arr.add(a);
+		arr.add(b);
 		this.n = n;
 	}
 	
@@ -46,17 +53,23 @@ class SyncFibonacciWriter implements Runnable{
 	
 	void add(int a, int b, int n) throws InterruptedException {
 		synchronized (arr) {
-			while(arr.size()!=n) {
-				c = a+b;
-				arr.add(c);
-				System.out.println("Added "+c+" to the list using "+Thread.currentThread().getName());
-//				System.out.println(a+" "+b+" "+c);
-				a = b;
-				b = c;
-				Thread.sleep(1000);
-				arr.notify();
+			try {
+				while(arr.size()!=n) {
+					c = a+b;
+					arr.add(c);
+					System.out.println("Added "+c+" to the list using "+Thread.currentThread().getName());
+					//				System.out.println(a+" "+b+" "+c);
+					a = b;
+					b = c;
+					Thread.sleep(500);
+					arr.notify();
+				}
+//				arr.wait();
+				Thread.currentThread().wait();
+				Thread.currentThread().interrupt();
+			}catch(InterruptedException ex) {
+				System.out.println(ex);
 			}
-			
 		}
 	}
 	
@@ -84,19 +97,26 @@ class SyncFibonacciReader implements Runnable{
 	
 	void read(int n) throws InterruptedException {
 		synchronized (arr) {
-			Thread.sleep(1000);
+			Thread.sleep(50);
 			for(int i = 0; i < n-2; i++) {
 				System.out.println(arr.get(i)+" + "+arr.get(i+1)+" = "+arr.get(i+2));
 			}
 			arr.notify();
 			
 		}
+//		arr.wait();
+		Thread.currentThread().wait();
+		Thread.currentThread().interrupt();
+//		arr.wait();
+//		System.exit(0);
 	}
 	
 }
 
+
 /*
-Output: 
+Output:
+
 Added 1 to the list using Writer
 Added 2 to the list using Writer
 Added 3 to the list using Writer
@@ -105,17 +125,28 @@ Added 8 to the list using Writer
 Added 13 to the list using Writer
 Added 21 to the list using Writer
 Added 34 to the list using Writer
-Added 55 to the list using Writer
-Added 89 to the list using Writer
+Exception in thread "Writer" java.lang.IllegalMonitorStateException: current thread is not owner
+	at java.base/java.lang.Object.wait(Native Method)
+	at java.base/java.lang.Object.wait(Object.java:321)
+	at javaIntroduction/wiley.training.advanced.day6.SyncFibonacciWriter.add(SyncFibonacci.java:68)
+	at javaIntroduction/wiley.training.advanced.day6.SyncFibonacciWriter.run(SyncFibonacci.java:46)
+	at java.base/java.lang.Thread.run(Thread.java:832)
+0 + 1 = 1
+1 + 1 = 2
 1 + 2 = 3
 2 + 3 = 5
 3 + 5 = 8
+Exception in thread "Reader" java.lang.IllegalMonitorStateException: current thread is not owner
+	at java.base/java.lang.Object.wait(Native Method)
+	at java.base/java.lang.Object.wait(Object.java:321)
+	at javaIntroduction/wiley.training.advanced.day6.SyncFibonacciReader.read(SyncFibonacci.java:108)
+	at javaIntroduction/wiley.training.advanced.day6.SyncFibonacciReader.run(SyncFibonacci.java:91)
+	at java.base/java.lang.Thread.run(Thread.java:832)
 5 + 8 = 13
 8 + 13 = 21
 13 + 21 = 34
-21 + 34 = 55
-34 + 55 = 89
+Printed Fibonacci
 
-*** The program doesn't terminate after this
 
+*** Want to resume in the main thread after both the threads are interrupted.
 */
