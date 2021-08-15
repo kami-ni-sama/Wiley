fd9d0ca8755ac3bdc3ae5b37ad2244414b2340c4
kami-ni-sama <ahmadjs310599@gmail.com>
2021-08-15 22:49:28 +0530

    Added count edges

--- DSA/DataStructures/GraphString.java
+++ DSA/DataStructures/GraphString.java
@@ -11,36 +11,6 @@ import java.util.List;
 
 
 public class GraphString {
-
-	class Node {
-		private String destination;
-		private int weight;
-		public Node(String destination) {
-			this.destination = destination;
-		}
-		public Node(String destination, int weight) {
-			super();
-			this.destination = destination;
-			this.weight = weight;
-		}
-		@Override
-		public String toString() {
-			return " [ "+destination + " " + weight + " ] ";
-		}
-		public String getDestination() {
-			return destination;
-		}
-		public int getWeight() {
-			return weight;
-		}
-		@Override
-		public boolean equals(Object obj) {
-			Node newNode = (Node) obj;
-			return this.destination.equals(newNode.getDestination());
-		}
-		
-		
-	}
 	
 	private Map<String, LinkedList<String>> map = new HashMap<String, LinkedList<String>>();
 
@@ -73,26 +43,6 @@ public class GraphString {
 	public int countVertices() {
 		return map.keySet().size();
 	}
-	
-//	public int countEdges() {
-//		int ctr = 0;
-//		ArrayList<String> visited = new ArrayList<String>();
-//		for(String key : map.keySet()) {
-//			visited.add(key);
-//			for(Node value : map.get(key)) {
-//				boolean flag = false;
-//				if(visited.contains(value.getDestination())) 
-//					for(Node node : map.get(value.getDestination()))
-//						if(node.equals(new Node(key))) {
-//							flag = true;
-//							break;
-//						}
-//				if(!flag)
-//					ctr++;
-//			}
-//		}
-//		return ctr;
-//	}
 	
 	public void bfs(String source) {
 		if(!map.containsKey(source)) {
@@ -134,15 +84,27 @@ public class GraphString {
 		}
 	}
 	
-//	public String traversal(String source) {
-//		if(!map.containsKey(source))
-//			return null;
-//		PriorityQueue<String> queue = new PriorityQueue<String>();
-//		queue.add(source);
-//		while(!queue.isEmpty()) {
-//			String source = 
-//		}
-//	}
-	
+	public int countEdges() {
+		int ctr = 0;
+		List<String> visited = new ArrayList<String>();
+		for(String source : map.keySet()) {
+			visited.add(source);
+			for(String destination : map.get(source)) {
+				boolean flag = false;
+				if(visited.contains(destination)) {
+					for(String temp : map.get(destination)) {
+						if(temp.equals(source)) {
+							flag = true;
+							break;
+						}
+					}
+				}
+				if(!flag)
+					ctr++;
+			}
+		}
+		return ctr;
+	}
+
 }
 
